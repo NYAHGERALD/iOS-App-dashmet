75cd02a771c996468387f2d747c4389ab6154b83
Gerald Nyah <59947415+NYAHGERALD@users.noreply.github.com>
2026-02-04 21:22:49 -0600

    Initial Xcode project

---
+++ MeetingIntelligence/MeetingIntelligenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingIntelligenceApp.swift
+//  MeetingIntelligence
+//
+//  Created by GERALD NYAH on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MeetingIntelligenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeetingIntelligenceTests/MeetingIntelligenceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingIntelligenceTests.swift
+//  MeetingIntelligenceTests
+//
+//  Created by GERALD NYAH on 2/4/26.
+//
+
+import Testing
+@testable import MeetingIntelligence
+
+struct MeetingIntelligenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
